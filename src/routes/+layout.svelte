a246d33f8911f0761544734b89506b124e8db9d4
Roti18 <akuronif@gmail.com>
2026-08-29 14:31:47 +0700

    refactor: apply application skeleton layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,37 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import '@fontsource/space-grotesk/400.css';
+	import '@fontsource/space-grotesk/500.css';
+	import '@fontsource/space-grotesk/600.css';
+	import '@fontsource/space-grotesk/700.css';
+	import '@fontsource/inter/400.css';
+	import '@fontsource/inter/500.css';
+	import '@fontsource/inter/600.css';
+	import '@fontsource/inter/700.css';
+	// import favicon from '../../static/favicon.ico';
 
+	import { page } from '$app/state';
+	import Header from '$lib/components/Header.svelte';
+	import Footer from '$lib/components/layout/Footer.svelte';
 	let { children } = $props();
+
+	const hideHeadrerRoutes = ['/login', '/register', '/forgot-password', '/reset-password'];
+
+	let shouldHidderHeader = $derived(hideHeadrerRoutes.includes(page.url.pathname));
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<svelte:head>
+	<link rel="icon" href="/favicon.svg" />
+</svelte:head>
+
+{#if !shouldHidderHeader}
+	<Header />
+{/if}
+
+<main>
+	{@render children()}
+</main>
+
+{#if page.url.pathname === '/'}
+	<Footer />
+{/if}
