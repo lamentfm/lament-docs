62a1baadbd76e9d665c8586b7c85e8daeab2ba6d
Roti18 <akuronif@gmail.com>
2026-08-29 14:32:02 +0700

    refactor: define type safety interface (docs)

---
+++ src/lib/types/docs.ts
@@ -0,0 +1,15 @@
+import type { Component } from 'svelte';
+
+export type MDFormatter = Record<string, unknown>;
+
+export type MDModule = {
+	default: Component;
+	metadata?: MDFormatter;
+};
+
+export type ChangeLogItem = {
+	version: string;
+	date?: string;
+	component?: Component;
+	metadata?: MDFormatter;
+};
