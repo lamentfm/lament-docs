9e11cd18ce5b5941c7be581d22a42c52fcaf43ea
Roti18 <akuronif@gmail.com>
2026-08-29 14:31:55 +0700

    feat: build documentation ui component (Steps)

---
+++ src/lib/components/docs/Steps.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let {
+		children
+	}: {
+		children?: import('svelte').Snippet;
+	} = $props();
+</script>
+
+<div class="my-6 space-y-0">
+	{#if children}
+		{@render children()}
+	{/if}
+</div>
