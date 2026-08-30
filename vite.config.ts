1ab91befea4e2266afaeaaf9c06b3a1e4d9d0a37
Roti18 <akuronif@gmail.com>
2026-08-30 10:30:11 +0700

    build(vite): configure @sveltejs/adapter-cloudflare in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import tailwindcss from '@tailwindcss/vite';
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
